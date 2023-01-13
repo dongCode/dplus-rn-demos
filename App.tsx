045b40aa4dba11602974614c4a9d61837125b4c7
D <dhy@jshxlsp.com>
2023-01-13 17:23:01 +0800

    feat: init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Images from '@dplus/images'
+import { travel } from './data/travel';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView  style={styles.container}>
+      <Text>Images</Text>
+      <Images data={travel.map(item => item.thumbnail)} imageContainerStyle={{ backgroundColor: 'green',height: 100}} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+  
   );
 }
 
